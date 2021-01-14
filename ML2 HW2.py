b913fd37e0fa07c2d6bdf398d9cd995cd669af36
joseph.schueder@collins.com <joseph.schueder@collins.com>
2021-01-14 14:18:02 -0600

    Update ML2 HW2.py

--- ML2 HW2.py
+++ ML2 HW2.py
@@ -15,35 +15,80 @@ import numpy as np
 from sklearn.metrics import accuracy_score # other metrics too pls!
 from sklearn.ensemble import RandomForestClassifier # more!
 from sklearn.model_selection import KFold
+# importing module  
+import csv 
+import urllib.request
+import requests 
+import io
 
 # adapt this code below to run your analysis
 # 1. Write a function to take a list or dictionary of clfs and hypers(i.e. use logistic regression), each with 3 different sets of hyper parameters for each
 # 2. Expand to include larger number of classifiers and hyperparameter settings
 # 3. Find some simple data
 
-
-# importing module  
-import csv 
-import urllib2
-
 url = 'https://raw.githubusercontent.com/jjschueder/DS7335MachineLearning2/main/Iris.csv'
-response = urllib2.urlopen(url)
-cr = csv.reader(response)  
-# opening the file using "with" 
-# statement 
-with open(open(filename, 'r') as data:      
-    for line in csv.DictReader(data): 
-        print(line) 
 
+def get_dict_wo_key(dictionary, keyp):
+    """Returns a **shallow** copy of the dictionary without a key."""
+    _dict = dictionary.copy()
+    for key in keyp:
+        #_dict.pop(key)
+        del _dict[key]
+    return _dict
+
+
+
+mylist = []
+r = requests.get(url)
+buff = io.StringIO(r.text)
+dr = csv.DictReader(buff)
+for row in dr:
+    print(row)
+    mylist.append(dict(row))
+    
+for d in mylist:
+    print(d['Species'])
+
+
+yremovals = ['Id', 'SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
+xremovals = ['Species', 'Id']
+
+for key in xremovals:
+    print("Value", key)
+
+
+xvalues = []
+for row in range(len(mylist)):
+    mylistx = get_dict_wo_key(mylist[row], xremovals)
+    xvalues.append(mylistx)
+  
+
+yvalues = []
+for row in range(len(mylist)):
+    mylisty = get_dict_wo_key(mylist[row], yremovals)
+    yvalues.append(mylisty)
+    
+X = []    
+for row in range(len(xvalues)):    
+    X.append(list(xvalues[row].values()))
+    
+Y = []    
+for row in range(len(yvalues)):    
+    Y.append(list(yvalues[row].values()))    
+
+XM = np.array(X)
+YL = np.array(Y) 
+
+   
 # 4. generate matplotlib plots that will assist in identifying the optimal clf and parampters settings
 # 5. Please set up your code to be run and save the results to the directory that its executed from
 # 6. Investigate grid search function
 
-M = np.array([[1,2],[3,4],[4,5],[4,5],[4,5],[4,5],[4,5],[4,5]])
-L = np.ones(M.shape[0])
+#M = np.array([[1,2],[3,4],[4,5],[4,5],[4,5],[4,5],[4,5],[4,5]])
+#L = np.ones(M.shape[0])
 n_folds = 5
 
-data = (M, L, n_folds)
+#data = (M, L, n_folds)
 
 def run(a_clf, data, clf_hyper={}):
   M, L, n_folds = data # unpack data container
@@ -60,5 +105,7 @@ def run(a_clf, data, clf_hyper={}):
                'accuracy': accuracy_score(L[test_index], pred)}
   return ret
 
-results = run(RandomForestClassifier, data, clf_hyper={})
+#results = run(RandomForestClassifier, data, clf_hyper={})
 #LongLongLiveGridS#LongLon#LLongLiveGridSearch!gLiveGridSearch!
+data_iris = (XM, YL.ravel(), n_folds)
+results_iris = run(RandomForestClassifier, data_iris, clf_hyper={})
